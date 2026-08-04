3312aa84f4bf68e3e7237cc2cd9cddc0bf794f0d
Nick van der Burgt <nvanderburgt@infiniot.nl>
2026-08-04 09:32:41 +0200

    Ensure start_time is in bounds of test time

--- src/main.py
+++ src/main.py
@@ -49,7 +49,7 @@ async def _generate_events() -> NewEvent | None:
     current_time_ams = datetime.now(ZoneInfo("Europe/Amsterdam"))
 
     # Start time is 12:00 today.
-    start_time = current_time_ams.replace(hour=12, minute=0, second=0, microsecond=0)
+    start_time = current_time_ams.replace(hour=6, minute=0, second=0, microsecond=0)
     # End time is 12:00 24 hours in the future
     end_time = start_time + timedelta(days=1)
 
